594c001b69488f3a61b136832781c468bbb99f33
MsAkiNom <monikasth22@gmail.com>
2022-05-02 15:41:09 +0300

    make it better

--- src/main/java/com/example/estonianpizzaaBE/DemoApplication.java
+++ src/main/java/com/example/estonianpizzaaBE/DemoApplication.java
@@ -2,10 +2,19 @@ package com.example.estonianpizzaaBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Tere, Maailm!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
