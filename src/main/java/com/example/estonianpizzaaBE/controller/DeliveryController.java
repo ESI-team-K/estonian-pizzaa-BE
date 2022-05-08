aee31472cf2e4c78e674547c1a9599e8fe31c4d8
Worraanong Chanchaijak <worraano@ut.ee>
2022-05-08 10:37:26 +0300

    setup delivery

---
+++ src/main/java/com/example/estonianpizzaaBE/controller/DeliveryController.java
@@ -0,0 +1,8 @@
+package com.example.estonianpizzaaBE.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeliveryController {
+
+}
--- src/main/java/com/example/estonianpizzaaBE/model/Delivery.java
+++ src/main/java/com/example/estonianpizzaaBE/model/Delivery.java
@@ -5,14 +5,63 @@ import java.time.Instant;
 public class Delivery {
 
     private final long deliveryId;
+    private final long driverId;
+    private final long estimateDeliveryTime; // minute
+    private final Instant startDeliveryTime;
+    private final Instant endDeliveryTime;
+    // TODO: Recipient model
+    private final String recipientName;
+    private final String recipientPhoneNumber;
+    private final String recipientAddress;
+    // TODO: Enum
+    private final String deliveryStatus;
 
-    public Delivery(long deliveryId) {
+    public Delivery(long deliveryId, long driverId, long estimateDeliveryTime, String recipientName,
+            String recipientPhoneNumber, String recipientAddress) {
         this.deliveryId = deliveryId;
-        Instant.now();
+        this.driverId = driverId;
+        this.estimateDeliveryTime = estimateDeliveryTime;
+        this.startDeliveryTime = Instant.now();
+        this.endDeliveryTime = null;
+        this.recipientName = recipientName;
+        this.recipientPhoneNumber = recipientPhoneNumber;
+        this.recipientAddress = recipientAddress;
+        this.deliveryStatus = "Waiting for a delivery person";
     }
 
     public long getId() {
         return deliveryId;
     }
 
+    public long getDriverId() {
+        return driverId;
+    }
+
+    public long getEstimateDeliveryTime() {
+        return estimateDeliveryTime;
+    }
+
+    public Instant getStartDeliveryTime() {
+        return startDeliveryTime;
+    }
+
+    public Instant getEndDeliveryTime() {
+        return endDeliveryTime;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public String getRecipientPhoneNumber() {
+        return recipientPhoneNumber;
+    }
+
+    public String getRecipientAddress() {
+        return recipientAddress;
+    }
+
+    public String getStatus() {
+        return deliveryStatus;
+    }
 }
