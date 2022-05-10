55e4f3655b72cc8b0bf007191b128eea55d5e617
Kaarel Ryysak <kaarel@ryysak.com>
2022-05-10 16:50:10 +0300

    small fix to cartitems

--- src/main/java/com/example/estonianpizzaaBE/model/MenuItem.java
+++ src/main/java/com/example/estonianpizzaaBE/model/MenuItem.java
@@ -34,6 +34,23 @@ public class MenuItem {
         this.name = name;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+    public void setSize(long size) {
+        this.size = size;
+    }
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
     public String getIngredients() {
         return ingredients;
     }
