627b06e1755ae5e9e16335e1f10ee4891228dafa
Kaarel Ryysak <kaarel@ryysak.com>
2022-05-13 14:46:53 +0300

    Created CORS placeholder

---
+++ src/main/java/com/example/estonianpizzaaBE/controller/CORSFilter.java
@@ -0,0 +1,38 @@
+package com.example.estonianpizzaaBE.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class CORSFilter implements Filter { // TODO: Remove this entire class when auth is implemented
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, 
+      FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "X-PINGOTHER,Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+        httpServletResponse.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // ...
+    }
+
+    @Override
+    public void destroy() {
+        // ...
+    }
+}
